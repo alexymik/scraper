a39bfe385b4a608356a2aaff95acfcf865bd987d
Alexy Mikhailichenko <alexy@gorges.us>
2016-07-05 09:09:08 -0400

    Better regex for full size images

--- example2.js
+++ example2.js
@@ -32,7 +32,7 @@ request(options, function (error, response, body) {
             'vehicle': vehicle,
             'date_added': date_added,
             'image': host + '/' + image,
-            //'full_image': host + '/' + image.replace(/resized_/g, '').replace(/&w=(\d+)/g, '')
+            //'full_image': host + '/new_photos/' + image.match(/(IMG_\d\d\d\d\.JPG)/g, '')
         })
     });
 
